467db4a137ad65a9d56fb5cd1572d69c9cd8b05d
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-06-30 12:17:32 -0300

    [12.0][IMP] helpdesk_mgmt_ticket_report

---
+++ helpdesk_mgmt_ticket_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 - TODAY, Escodoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Helpdesk Mgmt Ticket Report',
+    'summary': """
+        Helpdesk Ticket Report""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo,Odoo Community Association (OCA)',
+    'website': 'www.escodoo.com.br',
+    'depends': [
+        'helpdesk_mgmt',
+    ],
+    'data': [
+        'views/report_helpdesk_ticket.xml',
+    ],
+    'demo': [
+    ],
+}
